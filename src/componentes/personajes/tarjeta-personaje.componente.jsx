df00ca7c49481aec1137ea4d35cedfbeb8ca1943
Martin Rosas <mlrosas21@hotmail.com>
2023-04-03 22:27:13 -0300

    mark as fav func

--- src/componentes/personajes/tarjeta-personaje.componente.jsx
+++ src/componentes/personajes/tarjeta-personaje.componente.jsx
@@ -1,31 +1,38 @@
-import { useNavigate } from 'react-router-dom';
-import BotonFavorito from '../botones/boton-favorito.componente';
-import './tarjeta-personaje.css';
+import { useState } from "react";
+import { markAsFavorite } from "../../redux/charactersSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import BotonFavorito from "../botones/boton-favorito.componente";
+import "./tarjeta-personaje.css";
+import { Link } from "react-router-dom";
 
 /**
- * Tarjeta para cada personaje dentro de la grilla de personajes. 
- * 
+ * Tarjeta para cada personaje dentro de la grilla de personajes.
+ *
  * Deberás agregar las propiedades necesarias para mostrar los datos de los personajes
- * 
- * 
- * @returns un JSX element 
+ *
+ *
+ * @returns un JSX element
  */
-const TarjetaPersonaje = ({character}) => {
+const TarjetaPersonaje = ({ character }) => {
+    const favorites = useAppSelector(state => state.characters.favorites )
+    const dispatch = useAppDispatch()
 
-    const navigate = useNavigate()
-
-    const handleClick = (id) => {
-        console.log(id);
-        navigate(`/detalle/${id}`)
+    const handleClick = () => {
+        console.log("press");
+        dispatch(markAsFavorite(character.id))
     }
 
-    return <div className="tarjeta-personaje">
-        <img src={character.image} alt="Rick Sanchez" onClick={() => handleClick(character.id)}/>
-        <div className="tarjeta-personaje-body">
-            <span>{character.name}</span>
-            <BotonFavorito esFavorito={false} />
-        </div>
+  return (
+    <div className="tarjeta-personaje">
+      <Link to={`/detalle/${character.id}`}>
+        <img src={character.image} alt="Rick Sanchez" />
+      </Link>
+      <div className="tarjeta-personaje-body">
+        <span>{character.name}</span>
+        <BotonFavorito esFavorito={favorites.find(e => e === character.id) ? true : false} onClick={handleClick} />
+      </div>
     </div>
-}
+  );
+};
 
 export default TarjetaPersonaje;
--- src/redux/charactersSlice.ts
+++ src/redux/charactersSlice.ts
@@ -1,11 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { getAll, getCharacter} from "../api/character.queries";
+import { getAll, getCharacter } from "../api/character.queries";
 import { Character } from "../types/character.types";
 
-export const getCharacters = createAsyncThunk(
-  "characters/fetch",
-  getAll
-);
+export const getCharacters = createAsyncThunk("characters/fetch", getAll);
 
 export const getSingleCharacter = createAsyncThunk(
   "characters/fetchSingle",
@@ -16,18 +13,30 @@ interface CharactersState {
   characters: Character[];
   loading: boolean;
   selectedCharacter: Character | null;
+  favorites: number[];
 }
 
 const initialState: CharactersState = {
   characters: [],
   loading: false,
-  selectedCharacter: null
+  selectedCharacter: null,
+  favorites: []
 };
 
 const personajesSlice = createSlice({
   name: "characters",
   initialState,
-  reducers: {},
+  reducers: {
+    markAsFavorite: (state, action) => {
+      console.log(state.characters);
+      
+      if(!state.favorites.find(e => e === action.payload)){
+        state.favorites.push(action.payload)
+      } else {
+        state.favorites = state.favorites.filter(e => e !== action.payload)
+      }
+    },
+  },
   // Async calls
   extraReducers: (builder) => {
     builder
@@ -43,9 +52,10 @@ const personajesSlice = createSlice({
         state.selectedCharacter = null;
       })
       .addCase(getSingleCharacter.fulfilled, (state, action) => {
-        state.selectedCharacter = action.payload
-      })
+        state.selectedCharacter = action.payload;
+      });
   },
 });
 
+export const { markAsFavorite } = personajesSlice.actions;
 export default personajesSlice.reducer;
