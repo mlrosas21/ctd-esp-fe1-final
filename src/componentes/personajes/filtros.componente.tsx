e60d9403e4e641a48966edbd784b055bc0ed9597
Martin Rosas <mlrosas21@hotmail.com>
2023-04-05 22:45:14 -0300

    doc filter component

--- src/componentes/personajes/filtros.componente.tsx
+++ src/componentes/personajes/filtros.componente.tsx
@@ -1,5 +1,13 @@
 import './filtros.css';
 
+/**
+ * Functional component for filtering characters by name.
+ * @param {Object} props - The component props.
+ * @param {string} props.filter - The current filter value.
+ * @param {React.Dispatch<React.SetStateAction<string>>} props.setFilter - A function to update the filter value.
+ * @returns {JSX.Element} A JSX element that renders the filter UI.
+ */
+
 const Filtros = ({filter, setFilter}:{filter:string, setFilter:React.Dispatch<React.SetStateAction<string>>}) => {
     return <div className="filtros">
         <label htmlFor="nombre">Filtrar por nombre:</label>
