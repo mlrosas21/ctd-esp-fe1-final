c8d84020de21d7807ea249088113e79a6baa7ef7
Martin Rosas <mlrosas21@hotmail.com>
2023-04-05 22:26:12 -0300

    api queries documentation

--- src/api/characters.queries.ts
+++ src/api/characters.queries.ts
@@ -1,6 +1,23 @@
 import axios from 'axios'
 
+/**
+ * The base URL for the Rick and Morty API, character endpoint.
+ * @constant
+ * @type {string}
+ */
+
 const BASE_API_URL = 'https://rickandmortyapi.com/api/character'; 
+
+/**
+ * Fetches all characters from the API based on the given parameters.
+ *
+ * @async
+ * @function
+ * @param {Object} params - The parameters for the API call.
+ * @param {number} params.page - The page number to fetch.
+ * @param {string} params.filter - The name to filter characters by.
+ * @returns {Promise<Object[]>} - A promise that resolves to an array of character objects.
+ */
 
 export const getAll = async({ page, filter }: { page: number; filter: string }) => {
     let url = `${BASE_API_URL}?page=${page}`
@@ -10,6 +27,15 @@ export const getAll = async({ page, filter }: { page: number; filter: string }) 
     const response = await axios.get(url)
     return response.data.results
 }
+
+/**
+ * Fetches a single character from the API based on the given ID.
+ *
+ * @async
+ * @function
+ * @param {string} id - The ID of the character to fetch.
+ * @returns {Promise<Object>} - A promise that resolves to the character object.
+ */
 
 export const getCharacter = async(id: string) => {
     const response = await axios.get(`${BASE_API_URL}/${id}`)
--- src/api/episodes.queries.ts
+++ src/api/episodes.queries.ts
@@ -1,6 +1,21 @@
 import axios from 'axios'
 
+/**
+ * The base URL for the Rick and Morty API, episodes endpoint.
+ * @constant
+ * @type {string}
+ */
+
 const BASE_API_URL = 'https://rickandmortyapi.com/api/episode'; 
+
+/**
+ * Fetches multiple episodes from the API based on the given IDs.
+ *
+ * @async
+ * @function
+ * @param {string} ids - A comma-separated string of episode IDs to fetch.
+ * @returns {Promise<Object>} - A promise that resolves to an object containing episode data.
+ */
 
 export const getEpisodes = async(ids: string) => {
     const response = await axios.get(`${BASE_API_URL}/${ids}`)
